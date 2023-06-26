f896181753c574c3453c1008f610e51cd2608cec
Dmitriy Shishmintsev <dmitriy@Dmitriys-MacBook-Air.local>
2023-06-27 00:35:24 +0300

    golang server helloworld

---
+++ server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, `hello`)
+	})
+	http.ListenAndServe(":80", r)
+}
