65384ab0d0350f9eb0a246bde562e798bf555046
Frolower <frolowir@gmail.com>
2023-06-27 23:14:13 +0200

    pseudocode added to main.go

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	rooms "dicedasher/rooms"
+
 	"github.com/gorilla/mux"
 )
 
@@ -19,3 +20,31 @@ func main() {
 
 	http.ListenAndServe(":80", r)
 }
+
+/*
+
+import (
+    "math/rand"
+    "time"
+)
+
+var rooms = make(map[string]*Room)
+
+func CreateRoom () {
+	newID := GenerateUniqueId()
+
+	newRoom := &Room {
+		ID = newID
+		master = userID
+}
+
+func GenerateUniqueID () {
+	id := ""
+	seed := rand.NewSource(time.Now().UnixNano())
+	timeRand := rand.New(seed)
+	for i := 0; i < 10; i++ {
+		id += string(timeRand.Intn(58) + 65)
+	}
+}
+
+*/
