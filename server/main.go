aee23c7b0b0bc82b6612405421144b2b4ebe0bb6
Frolower <frolowir@gmail.com>
2023-06-27 14:56:33 +0200

    rooms package connected to main

--- server/main.go
+++ server/main.go
@@ -4,19 +4,18 @@ import (
 	"io"
 	"net/http"
 
-	rooms "./rooms"
+	rooms "dicedasher/rooms"
 	"github.com/gorilla/mux"
 )
 
-
 func main() {
 	r := mux.NewRouter()
-	
+
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, `hello`)
 	})
 
 	r.HandleFunc("/newroom", rooms.NewRoom)
-	
+
 	http.ListenAndServe(":80", r)
 }
