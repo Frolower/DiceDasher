29a5b82ece8f35eede36f0921ff6572241c34caa
Frolower <frolowir@gmail.com>
2023-06-29 21:26:34 +0200

    rollDice simplyfied

--- server/rooms/userActions/roll.go
+++ server/rooms/userActions/roll.go
@@ -5,90 +5,13 @@ import (
 	"time"
 )
 
-func rollD2() []int {
+func rollDice(size int) []int {
 	var result []int
 	seed := rand.NewSource(time.Now().UnixNano())
 	timeRand := rand.New(seed)
-	numIter := (timeRand.Intn(6) + 10)
-	for i := 0; i < numIter; i++ {
-		result = append(result, timeRand.Intn(2)+1)
-	}
-	return result
-}
-
-func rollD4() []int {
-	var result []int
-	seed := rand.NewSource(time.Now().UnixNano())
-	timeRand := rand.New(seed)
-	numIter := (timeRand.Intn(6) + 10)
+	numIter := (timeRand.Intn(size) + 10)
 	for i := 0; i < numIter; i++ {
 		result = append(result, timeRand.Intn(4)+1)
-	}
-	return result
-}
-
-func rollD6() []int {
-	var result []int
-	seed := rand.NewSource(time.Now().UnixNano())
-	timeRand := rand.New(seed)
-	numIter := (timeRand.Intn(6) + 10)
-	for i := 0; i < numIter; i++ {
-		result = append(result, timeRand.Intn(6)+1)
-	}
-	return result
-}
-
-func rollD8() []int {
-	var result []int
-	seed := rand.NewSource(time.Now().UnixNano())
-	timeRand := rand.New(seed)
-	numIter := (timeRand.Intn(6) + 10)
-	for i := 0; i < numIter; i++ {
-		result = append(result, timeRand.Intn(8)+1)
-	}
-	return result
-}
-
-func rollD10() []int {
-	var result []int
-	seed := rand.NewSource(time.Now().UnixNano())
-	timeRand := rand.New(seed)
-	numIter := (timeRand.Intn(6) + 10)
-	for i := 0; i < numIter; i++ {
-		result = append(result, timeRand.Intn(10)+1)
-	}
-	return result
-}
-
-func rollD12() []int {
-	var result []int
-	seed := rand.NewSource(time.Now().UnixNano())
-	timeRand := rand.New(seed)
-	numIter := (timeRand.Intn(6) + 10)
-	for i := 0; i < numIter; i++ {
-		result = append(result, timeRand.Intn(12)+1)
-	}
-	return result
-}
-
-func rollD20() []int {
-	var result []int
-	seed := rand.NewSource(time.Now().UnixNano())
-	timeRand := rand.New(seed)
-	numIter := (timeRand.Intn(6) + 10)
-	for i := 0; i < numIter; i++ {
-		result = append(result, timeRand.Intn(20)+1)
-	}
-	return result
-}
-
-func rollD100() []int {
-	var result []int
-	seed := rand.NewSource(time.Now().UnixNano())
-	timeRand := rand.New(seed)
-	numIter := (timeRand.Intn(6) + 10)
-	for i := 0; i < numIter; i++ {
-		result = append(result, timeRand.Intn(100)+1)
 	}
 	return result
 }
