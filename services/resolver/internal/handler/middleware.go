96887d2300594597552b16fdc54206d218eb013c
Nikita Dubinin <frolowir@gmail.com>
2026-03-06 18:43:05 -0800

    postgres.go migrated to be a project-wide service package

--- services/resolver/internal/handler/middleware.go
+++ services/resolver/internal/handler/middleware.go
@@ -1,15 +1,14 @@
 package handler
 
 import (
+	"diceDasher/pkg/dbutil"
 	"net/http"
-
-	"diceDasher/services/resolve/internal/repository"
 )
 
-func WithRepository(repo *repository.Repository) func(http.Handler) http.Handler {
+func WithRepository(repo *dbutil.Repository) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := repository.WithRepo(r.Context(), repo)
+			ctx := dbutil.WithRepo(r.Context(), repo)
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	}
