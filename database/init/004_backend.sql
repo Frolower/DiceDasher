ab8b87575b91806818e9200793637bc8e96adfc8
Nikita Dubinin <frolowir@gmail.com>
2026-09-12 00:25:48 +0300

    sessions table creation script added

--- database/init/004_backend.sql
+++ database/init/004_backend.sql
@@ -13,9 +13,23 @@ CREATE TABLE IF NOT EXISTS public.users (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE IF NOT EXISTS public.sessions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES public.users(id) ON DELETE CASCADE,
+    refresh_token_hash TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    expires_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() + INTERVAL '30 DAYS'),
+    revoked_at TIMESTAMPTZ,
+    user_agent TEXT
+);
+
 
 CREATE UNIQUE INDEX IF NOT EXISTS users_username_unique ON public.users (lower(username));
 CREATE UNIQUE INDEX IF NOT EXISTS users_email_unique ON public.users (lower(email));
 
 GRANT INSERT ON public.users TO user_service;
 GRANT SELECT (id) ON public.users TO user_service;
+GRANT DELETE ON public.users TO user_service;
+
+GRANT INSERT ON public.sessions TO user_service;
+GRANT SELECT (user_id) ON public.sessions TO user_service;
